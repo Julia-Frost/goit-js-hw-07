0d4669bc6fcb9258e9bc9564e4cf59c335e9bafa
Julia-Frost <juliafrostwork@gmail.com>
2022-08-21 11:53:54 +0300

    Add 1-st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+// Створення розмітки
+
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const list = createImgList(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", list);
+
+function createImgList(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="large-image.jpg">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>`;
+    })
+    .join("");
+}
+
+// Додання слухача подій на <div class="gallery">
+
+galleryEl.addEventListener("click", onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  // Підключення Лайтбокс
+  basicLightbox
+    .create(
+      `
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`
+    )
+    .show();
+}
